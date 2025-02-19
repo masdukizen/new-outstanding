0a5528c7c34f887d9c127534539cd5b932450291
sandysuy <suy932@gmail.com>
2025-02-20 02:58:16 +0700

    edit function token

--- lib/useToken.ts
+++ lib/useToken.ts
@@ -1,7 +1,11 @@
 export async function fetchToken(): Promise<string> {
   const tokenRes = await fetch("/api/auth/token", { credentials: "include" });
-  if (!tokenRes.ok) throw new Error("Unauthorized");
+  if (!tokenRes.ok) {
+    console.error("❌ Gagal mengambil token:", tokenRes.status);
+    throw new Error("Unauthorized");
+  }
 
   const { token } = await tokenRes.json();
+  console.log("✅ Token berhasil diambil:", token);
   return token;
 }
