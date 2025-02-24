d48af4e73e0ae75c335cbb44cd96d01d35e0d58d
sandysuy <suy932@gmail.com>
2025-02-25 05:51:57 +0700

    adit auth

--- auth.ts
+++ auth.ts
@@ -86,14 +86,14 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session;
     },
   },
-  // cookies: {
-  //   sessionToken: {
-  //     name: "__Secure-authjs.session-token", // Nama cookie
-  //     options: {
-  //       httpOnly: true, // Cookie hanya bisa diakses oleh server
-  //       secure: process.env.NODE_ENV === "production", // Hanya di produksi menggunakan HTTPS
-  //       sameSite: "none", // Harus None untuk cookie lintas domain
-  //     },
-  //   },
-  // },
+  cookies: {
+    sessionToken: {
+      name: "__Secure-authjs.session-token", // Nama cookie
+      options: {
+        httpOnly: true, // Cookie hanya bisa diakses oleh server
+        secure: process.env.NODE_ENV === "production", // Hanya di produksi menggunakan HTTPS
+        sameSite: "lax", // Harus None untuk cookie lintas domain
+      },
+    },
+  },
 });
