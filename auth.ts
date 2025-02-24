89cb91334eba6690e514f3512832c35c0d3ca1a6
sandysuy <suy932@gmail.com>
2025-02-25 05:35:05 +0700

    edit auth

--- auth.ts
+++ auth.ts
@@ -86,17 +86,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session;
     },
   },
-  cookies: {
-    sessionToken: {
-      name: "__Secure-authjs.session-token",
-      options: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "none",
-        path: "/",
-      },
-    },
-  },
   // cookies: {
   //   sessionToken: {
   //     name: "__Secure-authjs.session-token", // Nama cookie
